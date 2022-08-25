d2441ce79fcc2ef30d7be404ce20e33001a6f09a
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2022-08-25 14:25:28 +0200

    Update subnets.tf

--- subnets.tf
+++ subnets.tf
@@ -3,21 +3,23 @@ locals {
   
   vcn = local.tfe_workspace_outputs.vcn.vcn
   dmz_route_table = local.tfe_workspace_outputs.vcn.dmz_route_table
+  app_route_table = local.tfe_workspace_outputs.vcn.app_route_table
   
-  private_subnet = oci_core_subnet.private_subnet
+  app_subnet = oci_core_subnet.app_subnet
   dmz_subnet = oci_core_subnet.dmz_subnet
 }
 
-resource "oci_core_subnet" "private_subnet" {
+resource "oci_core_subnet" "app_subnet" {
   compartment_id = local.test_compartment.id
   vcn_id = local.vcn.id
   
-  display_name = "Private Network - Test Environment"
+  display_name = "Application Network - Test Environment"
   cidr_block = "10.0.10.0/24"
   prohibit_internet_ingress = true
+  route_table_id = local.app_route_table.id
   
   freeform_tags = merge({
-    security-profile = "private"
+    security-profile = "app"
     
   }, local.vcn.freeform_tags, local.test_compartment.freeform_tags )
 }
